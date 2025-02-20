7e2ff94293713ab9b758eaf59d08577462e404b6
Srinivasan <107301582+Srinivasan28111990@users.noreply.github.com>
2025-02-20 17:01:54 +0530

    Update emsjavascript.js
    Signed-off-by: Srinivasan <107301582+Srinivasan28111990@users.noreply.github.com>

--- emsjavascript.js
+++ emsjavascript.js
@@ -1,23 +1,10 @@
-const copyButton = document.getElementById("copyButton");
-
-
-
-copyButton.addEventListener("click", () => {
-
-  navigator.clipboard.writeText(copyButton.textContent)
-
-    .then(() => {
-
-      // Optional: Add a visual cue like an alert or a class change to indicate success
-
-      alert("Text copied to clipboard!");
-
-    })
-
-    .catch(err => {
-
-      console.error("Failed to copy text:", err);
-
-    });
-
-});
+function copyClipboard() {
+  var copyText = document.getElementById("myButton").innerHTML;
+  
+  var input = document.createElement("input");
+  input.value = copyText;
+  document.body.appendChild(input);
+  input.select();
+  document.execCommand("copy");
+  document.body.removeChild(input); 
+}
